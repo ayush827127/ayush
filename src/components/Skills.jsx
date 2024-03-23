b44bd2766ecb77f6323905dcd11f7d368eec3587
Aayush Gupta <ayush385361@gmail.com>
2024-03-23 12:38:49 +0530

    add: skill.module.css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,13 @@
 import {data} from "../Data/skillData"
 import SkillItem from "./SkillItem";
+import styles from "../css/Skill.module.css";
 
 function Skills() {
   return (
     <div className="container px-4  pt-2" id="skill">
       <h2 className="pb-2 border-bottom">Skills</h2>
       <hr style={{color: "cyan", height: "2px", backgroundColor: "cyan" }}/>
-      <div className="row g-4 py-5 row-cols-2 row-cols-lg-4 center" style={{"justifyContent": "center"}} >
+      <div className={`row g-4 py-5 row-cols-2 row-cols-lg-4 center ${styles.skillItemContainer} `} >
         {data.map((item) => (
         <SkillItem item={item[Object.keys(item)[0]]} key={item[Object.keys(item)[0]].name} />
         ))}
